f04370a6d440800bd896788bf4c17e8d0d862486
Adam Figgins <atfiggins@gmail.com>
2022-07-23 14:54:06 +0100

    feat(mpv): Adding code for making requests on omnisharp-lsp

---
+++ lua/omnisharp-lsp/client.lua
@@ -0,0 +1,56 @@
+local Client = {}
+
+function Client.get_omnisharp_client()
+  local clients = vim.lsp.buf_get_clients(0)
+  for _, client in pairs(clients) do
+    if client.name == "omnisharp" then
+      return client
+    end
+  end
+
+  print("'omnisharp' lsp client not attached to buffer. Please wait for client to be ready.")
+  error()
+end
+
+--- Make a request on the omnisharp server asynchronously
+---@param endpoint The omnisharp endpoint
+---@param params Request parameters
+---@param callback The callback to invoke with the response from the server
+---       when its ready. Format is function({err=<error_message>, result=<response>})
+function Client.make_request_async(endpoint, params, callback)
+  local status_ok, lsp_client = pcall(Client.get_omnisharp_client)
+
+  if not status_ok then
+    error("Omnisharp client is not attached. Cannot make request to server")
+  end
+
+  if lsp_client then
+    local status, id = lsp_client.request(endpoint, params, callback)
+
+    if not status then
+      vim.api.nvim_err_writeln("Error when executing " .. endpoint .. " : " .. id)
+      return
+    end
+  end
+end
+
+function Client.make_request(endpoint, params)
+  local status_ok, lsp_client = pcall(Client.get_omnisharp_client)
+
+  if not status_ok then
+    error()
+  end
+
+  if lsp_client then
+    local result, err = lsp_client.request_sync(endpoint, params, 100000)
+
+    if err then
+      vim.api.nvim_err_writeln("Error when executing " .. endpoint .. " : " .. err)
+      return
+    end
+
+    return result
+  end
+end
+
+return Client
