9dcd1547ca36d583b916cc43af621e2f50de49f8
Adam Figgins <atfiggins@gmail.com>
2022-08-31 18:41:40 +0100

    feat(console-output): Fixing console output to pick up results
    - Fixing console output for individual tests
    - Multiple test cases needs work
    - Updates treesitter queries to find additional tests

--- lua/neotest-dotnet/init.lua
+++ lua/neotest-dotnet/init.lua
@@ -36,7 +36,7 @@ DotnetNeotestAdapter.discover_positions = function(path)
     (class_declaration
       (attribute_list
         (attribute
-          name: (identifier) @attribute_name (#any-of? @attribute_name "TestClass")
+          name: (identifier) @attribute_name (#any-of? @attribute_name "TestClass" "TestFixture")
         )
       )
       name: (identifier) @namespace.name
@@ -61,7 +61,7 @@ DotnetNeotestAdapter.discover_positions = function(path)
     (method_declaration
       (attribute_list
         (attribute
-          name: (identifier) @attribute_name (#any-of? @attribute_name TestMethod)
+          name: (identifier) @attribute_name (#any-of? @attribute_name "TestMethod" "Test" "TestCase" "Fact" "Theory")
         )
       )
       name: (identifier) @test.name
@@ -91,25 +91,13 @@ DotnetNeotestAdapter.build_spec = function(args)
   end
 
   local project_dir = DotnetNeotestAdapter.root(position.path)
-  local result_file_name = "neotest-" .. os.date("%Y%m%d-%H%M%S") .. ".trx"
-  local result_path = Path:new(project_dir, "TestResults", result_file_name)
 
   -- This returns the directory of the .csproj or .fsproj file. The dotnet command works with the directory name, rather
   -- than the full path to the file.
   local test_root = project_dir
 
   local filter = ""
-  if position.type == "dir" then
-    return {}
-  end
-  if position.type == "file" then
-    -- TODO: Filename not specific enough to filter on with the match expression. Can be more robust depending on the
-    --      available filter expressions of the framework
-    filter = '--filter "FullyQualifiedName~' .. vim.fn.fnamemodify(position.name, ":r") .. '"'
-  end
   if position.type == "namespace" then
-    -- TODO: Namespace not specific enough, but will currenty run tests in namespaces with similar name
-    --     Better to figure out the test framework and then filter based on available filtering criteria for that specific framework
     filter = '--filter "FullyQualifiedName~' .. fqn .. '"'
   end
   if position.type == "test" then
@@ -139,13 +127,6 @@ DotnetNeotestAdapter.build_spec = function(args)
   }
 end
 
-local function remove_bom(str)
-  if string.byte(str, 1) == 239 and string.byte(str, 2) == 187 and string.byte(str, 3) == 191 then
-    str = string.sub(str, 4)
-  end
-  return str
-end
-
 local function get_test_nodes_data(tree)
   local test_nodes = {}
   for _, node in tree:iter_nodes() do
@@ -167,83 +148,18 @@ end
 ---@param b neotest.StrategyResult
 ---@param tree neotest.Tree
 ---@return neotest.Result[]
-DotnetNeotestAdapter.results = function(spec, result, tree)
-  -- From luarocks module
-
+DotnetNeotestAdapter.results = function(_, result, tree)
   local success, results = pcall(lib.files.read, result.output)
-  local test_nodes = get_test_nodes_data(tree)
 
   if not success then
     logger.error("No test output file found ")
     return {}
   end
 
-  if success then
-    result_utils.marshal_dotnet_console_output(results)
-    return {}
-  end
-
-  -- local no_bom_xml = remove_bom(xml)
-  local ok, parsed_data = pcall(lib.xml.parse, no_bom_xml)
-  if not ok then
-    logger.error("Failed to parse test output:", output_file)
-    return {}
-  end
-
-  local neotest_results = {}
-  local test_results = parsed_data.TestRun.Results
-
-  if #test_results.UnitTestResult > 1 then
-    test_results = test_results.UnitTestResult
-  end
-
-  if not test_results then
-    return {}
-  end
-
-  local file_result = { status = "passed", errors = {} }
-  for _, value in pairs(test_results) do
-    if value._attr.testName ~= nil then
-      local outcome = value._attr.outcome
-      local pos_id = spec.context.file_path .. "::" .. value._attr.testName
-      neotest_results[pos_id] = {
-        status = string.lower(outcome),
-        short = value._attr.testName .. ":" .. value._attr.outcome,
-        -- output = value.Output and value.Output.StdOut or "Passed",
-        errors = {},
-      }
-
-      if outcome == "Failed" then
-        local failure_message = value.Output.ErrorInfo.Message
-          .. "\n"
-          .. value.Output.ErrorInfo.StackTrace
-        -- neotest_results[pos_id].output = failure_message
-        table.insert(neotest_results[pos_id].errors, {
-          message = failure_message,
-        })
-
-        -- There is a failure, so set the overall file result status to failed and add errors
-        file_result.status = "failed"
-        vim.list_extend(file_result.errors, neotest_results[pos_id].errors)
-      end
-    end
-  end
-
-  neotest_results[spec.context.file_path] = file_result
-
-  local function get_namespace_results(node)
-    for parent in node:iter_parents() do
-      if parent:data().type ~= "namespace" then
-        neotest_results[parent:data().id] = file_result
-      end
-    end
-  end
-
-  for _, node in tree:iter_nodes() do
-    if node:data().type == "test" then
-      get_namespace_results(node)
-    end
-  end
+  local test_nodes = get_test_nodes_data(tree)
+  local intermediate_results = result_utils.marshal_dotnet_console_output(results)
+  local neotest_results =
+    result_utils.convert_intermediate_results(intermediate_results, test_nodes)
 
   return neotest_results
 end
--- lua/neotest-dotnet/result-utils.lua
+++ lua/neotest-dotnet/result-utils.lua
@@ -3,101 +3,67 @@ local async = require("neotest.async")
 
 local result_utils = {}
 local highlight_indicator = "\27[39;49m\27"
+local escaped_highlight_indicator = "\27%[39%;49m\27"
 local highlight_reset_matcher = "\27%[39%;49m"
-local failure_indicator = highlight_indicator .. "[91m  Failed"
-local failure_highlight = highlight_indicator .. "[91m"
-local pass_indicator = highlight_indicator .. "[92m  Passed"
-local pass_highlight = highlight_indicator .. "[92m"
-local skipped_indicator = highlight_indicator .. "[93m  Skipped"
-local skipped_highlight = highlight_indicator .. "[93m"
-local end_marker = "\r\n\r\n\27[39;49m\r\n\27[39;49m\27"
 
----@class IntermediateResults
----@field passed DotnetResult
----@field failed DotnetResult
----@field skipped DotnetResult
+local failure_indicator = highlight_indicator .. "[91m  Failed"
+local failure_highlight_escaped = escaped_highlight_indicator .. "%[91m"
+local failure_highlight = highlight_indicator .. "[91m"
+
+local pass_indicator = highlight_indicator .. "[92m  Passed"
+local pass_highlight_escaped = escaped_highlight_indicator .. "%[92m"
+local pass_highlight = highlight_indicator .. "[92m"
+
+local skipped_indicator = highlight_indicator .. "[93m  Skipped"
+local skipped_highlight_escaped = escaped_highlight_indicator .. "%[93m"
+local skipped_highlight = highlight_indicator .. "[93m"
 
 ---@class DotnetResult
 ---@field status string
 ---@field raw_output string
 ---@field test_name string
-local function gather_tests_by_outcome(output, test_outcome_indicator)
-  ---@type IntermediateResults
-  local intermediate_results = { passed = {}, failed = {}, skipped = {} }
-
-  local raw_outputs = {}
-  local search_idx = 0
-  while true do
-    local start_idx = string.find(output, pass_indicator, search_idx + 1, true)
-    local end_idx = -1
-
-    if start_idx then
-      end_idx = string.find(output, failure_indicator, search_idx + 1, true)
-        or string.find(output, skipped_indicator, search_idx + 1, true)
-        or string.find(output, end_marker, search_idx + 1, true)
-    else
-      start_idx = string.find(output, failure_indicator, search_idx + 1, true)
-    end
-
-    if start_idx then
-      end_idx = string.find(output, pass_indicator, search_idx + 1, true)
-        or string.find(output, skipped_indicator, search_idx + 1, true)
-        or string.find(output, end_marker, search_idx + 1, true)
-    end
-    if kk then
-    end
-    local end_idx = string.find(output, end_marker, start_idx + 1, true)
-      or string.find(output, failure_indicator, search_idx + 1, true)
-      or string.find(output, skipped_indicator, search_idx + 1, true)
-      or string.find(output, pass_indicator, search_idx + 1, true)
-    local result_str = string.sub(output, start_idx, end_idx or -1)
-    search_idx = end_idx or -1
-
-    table.insert(raw_outputs, result_str)
-    if search_idx == -1 then
-      -- End of the file
-      break
-    end
-  end
-
-  return raw_outputs
-end
 
 local function to_cr_lines(raw)
   return raw:gsub("\r", ""):gmatch("(.-)\n")
 end
 
+local function check_for_test_line(line)
+  local status = nil
+  local indicator = nil
+
+  if string.find(line, failure_indicator, 1, true) then
+    status = "failed"
+    indicator = failure_highlight
+  end
+  if string.find(line, pass_indicator, 1, true) then
+    status = "passed"
+    indicator = pass_highlight
+  end
+  if string.find(line, skipped_indicator, 1, true) then
+    status = "skipped"
+    indicator = skipped_highlight
+  end
+
+  return status, indicator
+end
+
 function result_utils.marshal_dotnet_console_output(raw)
-  put(raw)
   local output = to_cr_lines(raw)
 
-  ---@type IntermediateResults
-  local intermediate_results = { passed = {}, failed = {}, skipped = {} }
-  local current_status
-  local current_indicator
+  ---@type DotnetResult[]
+  local intermediate_results = {}
+  local current_status = nil
+  local current_indicator = nil
   local current_output = ""
-  local current_test_name = ""
+  local current_test_name = nil
 
   for line in output do
     if not current_status then
-      if string.find(line, failure_indicator, 1, true) then
-        current_status = "failed"
-        current_indicator = failure_highlight
-      end
-      if string.find(line, pass_indicator, 1, true) then
-        put("Got a passed")
-        put(line)
-        current_status = "passed"
-        current_indicator = pass_highlight
-      end
-      if string.find(line, skipped_indicator, 1, true) then
-        current_status = "skipped"
-        current_indicator = skipped_highlight
-      end
+      current_status, current_indicator = check_for_test_line(line)
 
       if current_status then
-        local start_idx, end_idx = string.find(line, current_indicator, 1, true)
-        current_test_name = string.match(line, "\27%[39%;49m([^%s]+)", end_idx)
+        local _, end_idx = string.find(line, current_indicator, 0, true)
+        current_test_name = string.match(line, highlight_reset_matcher .. "([^%s]+)", end_idx)
         current_output = current_output .. line
       end
     else
@@ -109,13 +75,22 @@ function result_utils.marshal_dotnet_console_output(raw)
           raw_output = current_output,
           test_name = current_test_name,
         }
-        table.insert(intermediate_results[current_status], result)
+        table.insert(intermediate_results, result)
 
-        -- Reset the current result to collect the next one
-        current_status = nil
-        current_indicator = nil
-        current_output = ""
-        current_test_name = ""
+        -- Check the line again. We may be at the beginning of the next test output
+        current_status, current_indicator = check_for_test_line(line)
+
+        if current_status then
+          local _, end_idx = string.find(line, current_indicator, 0, true)
+          current_test_name = string.match(line, "\27%[39%;49m([^%s]+)", end_idx)
+          current_output = line
+        else
+          -- Reset the current result to collect the next one
+          current_status = nil
+          current_indicator = nil
+          current_output = ""
+          current_test_name = nil
+        end
       end
     end
   end
@@ -125,67 +100,41 @@ function result_utils.marshal_dotnet_console_output(raw)
 end
 
 ---Converts and adds the results of the test_results list to the neotest_results table.
----@param neotest_results neotest.Result[]
----@param test_results table<string, any>
----@param file_path string
----@return table<string, any>
-function result_utils.convert_intermediate_results(output, test_nodes)
+---@param intermediate_results DotnetResult[] The marshalled dotnet console outputs
+---@param test_nodes neotest.Tree
+---@return neotest.Result[]
+function result_utils.convert_intermediate_results(intermediate_results, test_nodes)
   local neotest_results = {}
 
-  local output_lines = to_cr_lines(output)
-
-  -- Gather raw output of each test in the run
-  local passed_raw = gather_tests_by_outcome(output, pass_indicator)
-  local failed_raw = gather_tests_by_outcome(output, failure_indicator)
-  local skipped_raw = gather_tests_by_outcome(output, skipped_indicator)
-
-  for _, raw_output in ipairs(passed_raw) do
+  for _, result in ipairs(intermediate_results) do
     for _, node in ipairs(test_nodes) do
-      if string.find(raw_output, node.name) then
+      if result.test_name == node.name then
+        local sanitized_output = result.raw_output
+          :gsub(failure_highlight_escaped, "")
+          :gsub(pass_highlight_escaped, "")
+          :gsub(skipped_highlight_escaped, "")
+          :gsub(escaped_highlight_indicator, "")
+          :gsub(highlight_reset_matcher, "")
+
         local fname = async.fn.tempname()
-        vim.fn.writefile({ raw_output }, fname, "b")
+        async.fn.writefile({ result.raw_output }, fname)
         neotest_results[node.id] = {
-          status = "passed",
+          status = result.status,
           short = node.name .. ":Passed",
           output = fname,
           errors = {},
         }
-        break
-      end
-    end
-  end
 
-  for _, raw_output in ipairs(failed_raw) do
-    for _, node in ipairs(test_nodes) do
-      if string.find(raw_output, node.name) then
-        local fname = async.fn.tempname()
-
-        -- Testing raw output
-        put(raw_output)
-        local CR_RAW = to_cr_lines(raw_output)
-        local error_message = ""
-        local error_lines = {}
-        for line in CR_RAW do
-          error_message = error_message .. line .. "\n"
-          table.insert(error_lines, line)
+        if result.status == "failed" then
+          table.insert(neotest_results[node.id].errors, {
+            message = sanitized_output,
+          })
         end
-        async.fn.writefile(error_lines, fname)
-        neotest_results[node.id] = {
-          status = "failed",
-          short = node.name .. ":Failed",
-          output = fname,
-          errors = {
-            {
-              message = error_message,
-            },
-          },
-        }
         break
       end
     end
   end
 
-  put(neotest_results)
   return neotest_results
 end
 
