74649fca140ce79da23ee32112cac62a1ebc0e69
Adam Figgins <atfiggins@gmail.com>
2022-09-01 18:34:45 +0100

    fix(ts-queries): Fixing name of unit test query file

---
+++ lua/neotest-dotnet/tree-sitter/unit-test-queries.lua
@@ -0,0 +1,21 @@
+return [[
+  ;; Matches test classes
+  (class_declaration
+    (attribute_list
+      (attribute
+        name: (identifier) @attribute_name (#any-of? @attribute_name "TestClass" "TestFixture")
+      )
+    )
+    name: (identifier) @namespace.name
+  ) @namespace.definition
+
+  ;; Matches test methods
+  (method_declaration
+    (attribute_list
+      (attribute
+        name: (identifier) @attribute_name (#any-of? @attribute_name "TestMethod" "Test" "TestCase" "Fact" "Theory")
+      )
+    )
+    name: (identifier) @test.name
+  ) @test.definition
+]]
