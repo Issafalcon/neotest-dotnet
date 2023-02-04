009c896807d40599dfe001ffcf520f0b58cd5011
Adam Figgins <adam.figgins@gmail.com>
2023-02-04 13:31:36 +0000

    chore(tests): Adding addiitonal tests for build_spec
    - Failing at the moment, but can be fixed by using Tree obj functions

--- tests/build_spec_utils_spec.lua
+++ tests/build_spec_utils_spec.lua
@@ -1,9 +1,10 @@
 local async = require("plenary.async.tests")
 local mock = require("luassert.mock")
+local stub = require("luassert.stub")
+local lib = require("neotest.lib")
 
 describe("build_test_fqn windows_os", function()
   local BuildSpecUtils = require("neotest-dotnet.build-spec-utils")
-  local mock = require("luassert.mock")
   local fn_mock = mock(vim.fn, true)
   fn_mock.has.returns(true)
 
@@ -26,6 +27,7 @@ describe("build_test_fqn windows_os", function()
       assert.are.equals(fqn, "namespace.class.method")
     end
   )
+  mock.revert(fn_mock)
 end)
 
 describe("build_test_fqn linux", function()
@@ -52,4 +54,56 @@ describe("build_test_fqn linux", function()
       assert.are.equals(fqn, "namespace.class.method")
     end
   )
+  mock.revert(fn_mock)
+end)
+
+describe("create_specs", function()
+  local BuildSpecUtils = require("neotest-dotnet.build-spec-utils")
+  local test_result_path = "/tmp/test_result.trx"
+  -- local lib = require("neotest.lib")
+  -- local lib_mock = mock(lib.files, true)
+  -- fn_mock.tempname.returns(test_result_path)
+  -- lib_mock.files.match_root_pattern.returns(function(path)
+  --   return "test"
+  -- end)
+  before_each(function()
+    stub(lib.files, "match_root_pattern", function(_)
+      return function(_)
+        return "test"
+      end
+    end)
+  end)
+
+  it("should return correct spec when position is 'file' type", function()
+    local expected_specs = {
+      {
+        command = 'dotnet test /home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2  --results-directory /tmp/nvim.issafalcon/794d7y --logger "trx;logfilename=0.trx"',
+        context = {
+          file = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
+          results_path = test_result_path,
+        },
+      },
+    }
+
+    -- TODO: Get this tree using the 'from_list' function on the Tree obj
+    local tree = {
+      "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
+      adapter = "neotest-dotnet:/home/issafalcon/repos/neotest-dotnet-tests",
+      strategy = "integrated",
+      tree = {
+        _data = {
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
+          name = "UnitTest1.cs",
+          path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
+          range = { 0, 0, 27, 0 },
+          type = "file",
+        },
+      },
+    }
+
+    local result = BuildSpecUtils.create_specs(tree)
+
+    assert.equal(#expected_specs, #result)
+  end)
 end)
