b81a3ee920e2b3505a7549e073dc56817da443e3
Adam Figgins <adam.figgins@gmail.com>
2023-02-05 09:08:28 +0000

    chore(sln-root): Adding more unit tests for build spec

--- tests/build_spec_utils_spec.lua
+++ tests/build_spec_utils_spec.lua
@@ -2,6 +2,12 @@ local async = require("plenary.async.tests")
 local mock = require("luassert.mock")
 local stub = require("luassert.stub")
 local lib = require("neotest.lib")
+local neotest_async = require("neotest.async")
+local Tree = require("neotest.types").Tree
+
+A = function(...)
+  print(vim.inspect(...))
+end
 
 describe("build_test_fqn windows_os", function()
   local BuildSpecUtils = require("neotest-dotnet.build-spec-utils")
@@ -59,51 +65,134 @@ end)
 
 describe("create_specs", function()
   local BuildSpecUtils = require("neotest-dotnet.build-spec-utils")
-  local test_result_path = "/tmp/test_result.trx"
-  -- local lib = require("neotest.lib")
-  -- local lib_mock = mock(lib.files, true)
-  -- fn_mock.tempname.returns(test_result_path)
-  -- lib_mock.files.match_root_pattern.returns(function(path)
-  --   return "test"
-  -- end)
+  local test_result_path = "/tmp/output/test_result"
+  local test_root_path = "/dummy/path/to/proj"
+
+  local function assert_spec_matches(expected, actual)
+    assert.equal(expected.command, actual.command)
+    assert.equal(expected.context.file, actual.context.file)
+    assert.equal(expected.context.id, actual.context.id)
+    assert.equal(expected.context.results_path, actual.context.results_path)
+  end
+
   before_each(function()
+    -- fn_mock.tempname.returns(test_result_path)
+
+    stub(vim.fn, "tempname", function()
+      return test_result_path
+    end)
     stub(lib.files, "match_root_pattern", function(_)
       return function(_)
-        return "test"
+        return test_root_path
       end
     end)
+  end)
+
+  after_each(function()
+    lib.files.match_root_pattern:revert()
+    vim.fn.tempname:revert()
   end)
 
   it("should return correct spec when position is 'file' type", function()
     local expected_specs = {
       {
-        command = 'dotnet test /home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2  --results-directory /tmp/nvim.issafalcon/794d7y --logger "trx;logfilename=0.trx"',
+        command = "dotnet test "
+          .. test_root_path
+          .. '  --results-directory /tmp/output --logger "trx;logfilename=test_result.trx"',
         context = {
-          file = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
-          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
-          results_path = test_result_path,
+          file = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          results_path = test_result_path .. ".trx",
         },
       },
     }
 
-    -- TODO: Get this tree using the 'from_list' function on the Tree obj
-    local tree = {
-      "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
-      adapter = "neotest-dotnet:/home/issafalcon/repos/neotest-dotnet-tests",
-      strategy = "integrated",
-      tree = {
-        _data = {
-          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
-          name = "UnitTest1.cs",
-          path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj2/UnitTest1.cs",
-          range = { 0, 0, 27, 0 },
-          type = "file",
+    local tree = Tree.from_list({
+      {
+        id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+        name = "UnitTest1.cs",
+        path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+        range = { 0, 0, 19, 0 },
+        type = "file",
+      },
+      {
+        {
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1",
+          name = "xunit.testproj1",
+          path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          range = { 0, 0, 18, 1 },
+          type = "namespace",
         },
       },
-    }
+    }, function(pos)
+      return pos.id
+    end)
 
     local result = BuildSpecUtils.create_specs(tree)
 
     assert.equal(#expected_specs, #result)
+    assert_spec_matches(expected_specs[1], result[1])
+  end)
+
+  async.it("should return the correct specs when the position is 'namespace' type", function()
+    local expected_specs = {
+      {
+        command = 'dotnet test /home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1  --results-directory /tmp/nvim.issafalcon/wFuRtz --logger "trx;logfilename=0.trx"',
+        context = {
+          file = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1",
+          results_path = "/tmp/nvim.issafalcon/wFuRtz/0.trx",
+        },
+      },
+    }
+
+    local expected_specs = {
+      {
+        command = "dotnet test "
+          .. test_root_path
+          .. ' --filter FullyQualifiedName~"xunit.testproj1"'
+          .. ' --results-directory /tmp/output --logger "trx;logfilename=test_result.trx"',
+        context = {
+          file = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1",
+          results_path = test_result_path .. ".trx",
+        },
+      },
+    }
+
+    local tree = Tree.from_list({
+      {
+        id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1",
+        name = "xunit.testproj1",
+        path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+        range = { 0, 0, 18, 1 },
+        type = "namespace",
+      },
+      {
+        {
+          id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1::UnitTest1",
+          name = "UnitTest1",
+          path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+          range = { 2, 0, 18, 1 },
+          type = "namespace",
+        },
+        {
+          {
+            id = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs::xunit.testproj1::UnitTest1::Test1",
+            name = "Test1",
+            path = "/home/issafalcon/repos/neotest-dotnet-tests/xunit/testproj1/UnitTest1.cs",
+            range = { 4, 1, 8, 2 },
+            type = "test",
+          },
+        },
+      },
+    }, function(pos)
+      return pos.id
+    end)
+
+    local result = BuildSpecUtils.create_specs(tree)
+
+    assert.equal(#expected_specs, #result)
+    assert_spec_matches(expected_specs[1], result[1])
   end)
 end)
