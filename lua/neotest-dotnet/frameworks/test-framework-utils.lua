43e658493601119ec10f0074f90c340c8b94d9ce
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-04-18 14:12:29 +0200

    Update lua/neotest-dotnet/frameworks/test-framework-utils.lua
    Co-authored-by: Jesper Lundberg <91946876+JesLun@users.noreply.github.com>

--- lua/neotest-dotnet/frameworks/test-framework-utils.lua
+++ lua/neotest-dotnet/frameworks/test-framework-utils.lua
@@ -34,7 +34,9 @@ function FrameworkUtils.get_test_framework_utils(source, custom_attribute_args)
 
   async.scheduler()
   local root = vim.treesitter.get_string_parser(source, "c_sharp"):parse()[1]:root()
-  local parsed_query = vim.fn.has("nvim-0.9.0") == 1 and vim.treesitter.query.parse("c_sharp", framework_query) or vim.treesitter.parse_query("c_sharp", framework_query)
+  local parsed_query = vim.fn.has("nvim-0.9.0") == 1
+      and vim.treesitter.query.parse("c_sharp", framework_query)
+    or vim.treesitter.parse_query("c_sharp", framework_query)
   for _, captures in parsed_query:iter_matches(root, source) do
     local test_attribute = vim.fn.has("nvim-0.9.0") == 1 and vim.treesitter.get_node_text(captures[1], source) or vim.treesitter.query.get_node_text(captures[1], source)
     if test_attribute then
