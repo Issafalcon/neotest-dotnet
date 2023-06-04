81942459d9387b4b2bbb28716b281838b6361a9d
Adam Figgins <adam@pop-os.localdomain>
2023-06-04 17:46:50 +0100

    fix(treesitter): Further attempt to fix backwards compatibil;ity

--- lua/neotest-dotnet/frameworks/nunit.lua
+++ lua/neotest-dotnet/frameworks/nunit.lua
@@ -31,7 +31,7 @@ M.build_parameterized_test_positions = function(base_node, source, captured_node
   ]]
 
   local param_query = vim.fn.has("nvim-0.9.0") == 1 and vim.treesitter.query.parse("c_sharp", query)
-    or vim.treesitter.parse_query(query, "c_sharp")
+    or vim.treesitter.parse_query("c_sharp", query)
 
   -- Set type to test (otherwise it will be test.parameterized)
   local parameterized_test_node = vim.tbl_extend("force", base_node, { type = "test" })
